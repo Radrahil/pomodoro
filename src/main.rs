3119881421c21ead09ba4c5bd86bc986d62cb8cb
Rahil Prakash <54015030+Radrahil@users.noreply.github.com>
2024-04-17 19:39:45 +0530

    Initial semi-useable state

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,72 @@
+use std::io::{stdout, Write};
 use std::process::Command;
-use std::time::Instant;
+use std::thread;
+use std::time::{Duration, Instant};
 
 fn main() {
-    let now = Instant::now();
+    let work = Instant::now();
+    let five_secs = Duration::from_secs(62);
 
-    // after 5 seconds sends a notification.
+    let mut stdout = stdout();
+
     loop {
-        if now.elapsed().as_secs() > 5 {
+        if work.elapsed() > five_secs {
             Command::new("notify-send")
                 .arg("5 seconds have passed")
                 .output()
                 .expect("failed to execute process");
-            Command::new("spd-say")
-                .arg("5 seconds have passed")
-                .output()
-                .expect("failed to execute process");
-            break;
+
+            write!(stdout, "\rTime left: 00:00\n").unwrap();
+            stdout.flush().unwrap(); // Flush buffer to ensure immediate output
+            println!("Break time!");
+
+            break; // Exit loop once the condition is met
+        } else {
+            // Print time left
+            let elapsed_time = work.elapsed().as_secs();
+            let minutes_left = 25 - elapsed_time.div_euclid(60) - 1;
+            let seconds_left = 60 - elapsed_time.rem_euclid(60) - 1;
+            write!(
+                stdout,
+                "\rTime left: {:02}:{:02}",
+                minutes_left, seconds_left
+            )
+            .unwrap();
+            stdout.flush().unwrap(); // Flush buffer to ensure immediate output
+
+            thread::sleep(Duration::from_millis(500)); // Sleep for a short duration
         }
     }
+
+    // Wait for 5 minutes
+    thread::sleep(Duration::from_secs(5));
+    Command::new("notify-send")
+        .arg("Another 5 seconds have passed")
+        .output()
+        .expect("failed to execute process");
+    println!("Break over");
 }
+
+// use std::{thread, time};
+// use std::io::{Write, stdout};
+// use crossterm::{QueueableCommand, cursor, terminal, ExecutableCommand};
+//
+// fn main() {
+//     let mut stdout = stdout();
+//
+//     stdout.execute(cursor::Hide).unwrap();
+//     for i in (1..30).rev() {
+//         stdout.queue(cursor::SavePosition).unwrap();
+//         stdout.write_all(format!("{}: FOOBAR ", i).as_bytes()).unwrap();
+//         stdout.queue(cursor::RestorePosition).unwrap();
+//         stdout.flush().unwrap();
+//         thread::sleep(time::Duration::from_millis(100));
+//
+//         stdout.queue(cursor::RestorePosition).unwrap();
+//         stdout.queue(terminal::Clear(terminal::ClearType::CurrentLine)).unwrap();
+//     }
+//     stdout.execute(cursor::Show).unwrap();
+//
+//     println!("Done!");
+// }
+//
